9f7920d5b0e188948c9153f1c3ba175fbde425dc
Jargalsuren <jargalsuren@uni.minerva.edu>
2022-12-08 17:53:52 -0300

    separation of concern, recipe

--- website/models.py
+++ website/models.py
@@ -8,3 +8,16 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
 
+class Ingredients(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    ingredient = db.Column(db.String(50), nullable=False)
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150), unique=False, nullable=False)
+    category = db.Column(db.String(20))
+    photo = db.Column(db.String(100))
+    duration = db.Column(db.Integer, unique=False)
+    instruction = db.Column(db.String(10000), nullable=True)
+
+
