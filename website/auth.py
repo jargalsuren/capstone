c1cc7c4582a38091c38744c336200624a45bc6e5
Jargalsuren <jargalsuren@uni.minerva.edu>
2022-12-08 18:01:58 -0300

    fixes

--- website/auth.py
+++ website/auth.py
@@ -49,7 +49,7 @@ def sign_up():
         elif len(email) < 4:
             flash('Email must be greater than 3 characters.', category='error')
         elif len(first_name) < 2:
-            flash('First name must be greater than 1 character.', category='error')
+            flash('First name must be more than 1 character.', category='error')
         elif password1 != password2:
             flash('Passwords don\'t match.', category='error')
         elif len(password1) < 7:
